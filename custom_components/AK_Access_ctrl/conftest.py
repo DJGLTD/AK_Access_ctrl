3926fba49f9ea363956cab56a0eb91f1d52bd936
Daniel <120346651+DJGLTD@users.noreply.github.com>
2026-01-07 07:15:19 +0000

    Remove redundant AK_Access_ctrl copy

--- custom_components/AK_Access_ctrl/conftest.py
+++
@@ -1,5 +0,0 @@
-"""Global pytest configuration ensuring Home Assistant stubs are available."""
-
-from ha_test_stubs import ensure_homeassistant_stubs
-
-ensure_homeassistant_stubs()
--- custom_components/AK_Access_ctrl/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Test suite for the AK Access control integration."""
